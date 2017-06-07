df285fdc247b272d0624902f70e0654058587c41
UberKitten <566438+UberKitten@users.noreply.github.com>
2017-06-07 14:55:12 -0500

    Reorganize js into multiple files

---
+++ js/barcowned-models.js
@@ -0,0 +1,147 @@
+var models = {
+	symbolds6707: {
+		/*
+			Example of a `1 rule that types Win+R, cmd.exe, the barcode, then enter
+			"^FNC380",			// erase all rules
+			"^FNC37B1211",		// begin new rule
+			"^FNC36C201",		// specific string at start (max 8)
+			"^FNC3B60",			// `
+			"^FNC3B31",			// 1
+			"^FNC3B+",			// end of message
+			"^FNC330C0D20063",	// specify pause duration
+			"^FNC3A1",			// 1
+			"^FNC3A0",			// 0 (1.0 second delay)
+			"^FNC36A144352",	// send Win + R
+			"^FNC36A118",		// send pause
+			"^FNC3B63",			// c
+			"^FNC3B6D",			// m
+			"^FNC3B64",			// d
+			"^FNC36A14470D",	// send enter
+			"^FNC36A118",		// send pause
+			"^FNC36A143302",	// skip ahead 2 characters (ignoring `1 at start)
+			"^FNC36A110",		// send all data that remains in barcode
+			"^FNC36A14470D",	// send enter
+			"^FNC34"			// save rule
+		*/
+
+		name: "Symbol DS6707",
+		autodelay: 500, // roughly how fast we can send barcodes when autoscanning
+		setup: {
+			symbology: "code128",
+			prefix: "^FNC3",
+			postfix: "",
+			enterconfig: "",
+			exitconfig: "",
+			options: {
+				scanpresentation: "2050207", // Presentation mode scanning ("Blink")
+				enableparameterscanning: "1040601", // Enable parameter scanning
+				mobilephonedecode: [
+					"N02CC03", // Mobile Phone Decode Enable
+					"N02D60D", // Mobile Phone Decode High Aggressive
+				],
+				eraseallrules: "80" // Erase all rules
+			}
+		},
+		adf: {
+			symbology: "code128",
+			prefix: "^FNC3",
+			postfix: "",
+			enterconfig: "7B1211", // begin new rule
+			exitconfig: "4", // save rule
+			endmessage: "B+", // used after sending text
+			criteria: {
+				stringatposition: {
+					type: "charmap",
+					sendendmessage: true,
+					enterconfig: "6C200",
+					exitconfig: "",
+					prefix: "B",
+					postfix: ""
+				},
+				stringatstart: {
+					type: "charmap",
+					sendendmessage: true,
+					enterconfig: "6C201",
+					exitconfig: "",
+					prefix: "B",
+					postfix: ""
+				},
+				stringsearch: {
+					type: "charmap",
+					sendendmessage: true,
+					enterconfig: "6C202",
+					exitconfig: "",
+					prefix: "B",
+					postfix: ""
+				}
+			},
+			actions: {
+				sendtext: {
+					type: "charmap", // each char in input creates a new barcode
+					prefix: "B"
+				},
+				sendcontrol: {
+					type: "multiple", // each char in input creates a new barcode, runs process with one char
+					sendendmessage: true,
+					prefix: "6A1441",
+					process: function(input, adf, callback) { // TODO: change this to callback instead of return
+						// example output: 2=00 A=01 B=02 Z=1A [=1B
+						// non-alphabet
+						if (input == "2") callback("00");
+						if (input == "[") callback("1B");
+						if (input == "\\") callback("1C");
+						if (input == "]") callback("1D");
+						if (input == "6") callback("1E");
+						if (input == "-") callback("1F");
+						
+						input = input.toUpperCase().charCodeAt(0); // convert to ASCII code
+						if (input >= 65 && input <= 90) { // A-Z
+							callback(pad((input - 64).toString(16), 2, "0")); // wait need to convert to hex
+						}
+					}
+				},
+				pauseduration: {
+					type: "single",
+					enterconfig: "30C0D20063",
+					prefix: "A",
+					process: function(input, adf, callback) {
+						// 1.0 duration would be A1, A0
+						callback(Math.floor(input));
+						callback(Math.floor((input % 1).toFixed(1) * 10));
+					}
+				},
+				sendgui: {
+					type: "charmap",
+					prefix: "6A1443"
+				},
+				sendpause: "6A118",
+				sendenter: "6A14470D",
+				sendremaining: "6A110",
+				skipcharacters: {
+					type: "single",
+					prefix: "6A1433",
+					process: function(input, adf, callback) {
+						callback(pad(input, 2, "0"));
+					}
+				}
+			},
+			mapcharacter: {
+				type: "multiple", // each char in input creates a new barcode, runs process with one char
+				process: function(input, adf, callback) { // ADF doesn't take normal keys...
+					// example output: space=20 #=23 $=24 +=2B
+					// straight hex of ASCII
+					callback(pad(input.charCodeAt(0).toString(16).toUpperCase(), 2, "0"));
+				}
+			}
+			
+			
+		},
+		bwippoptions: {
+			parsefnc: true,
+			includetext: true // temp until bwipp inheritance added
+		}
+		
+		// "Using ADF with longer bar codes transmits the bar code in segments of length 252 or less (depending on the host selected), and applies the rule to each segment."
+		// TODO: Parameter to define split support
+	}
+};
--- js/barcowned.js
+++ js/barcowned.js
@@ -1,15 +1,11 @@
-$(function  () {
-  $("ol.example").sortable();
-});
-
-var bw;
-var barcodeNumber = 0;
-
-var config = {
-	bwippoptions: { // these apply globally and override model settings
-		includetext: true
-	}
-};
+/*
+	TODO: implement this
+	var config = {
+		bwippoptions: { // these apply globally and override model settings
+			includetext: true
+		}
+	};
+*/
 
 // https://stackoverflow.com/a/16122676/415943
 function pad(string, length, pad_char, append) {
@@ -21,193 +17,6 @@ function pad(string, length, pad_char, append) {
 		string = append ? string+pad_char : pad_char+string;
 	}
 	return string;
-}
-
-var models = {
-	symbolds6707: {
-		name: "Symbol DS6707",
-		autodelay: 500, // roughly how fast we can send barcodes when autoscanning
-		setup: {
-			symbology: "code128",
-			prefix: "^FNC3",
-			postfix: "",
-			enterconfig: "",
-			exitconfig: "",
-			options: {
-				scanpresentation: "2050207", // Presentation mode scanning ("Blink")
-				enableparameterscanning: "1040601", // Enable parameter scanning
-				mobilephonedecode: [
-					"N02CC03", // Mobile Phone Decode Enable
-					"N02D60D", // Mobile Phone Decode High Aggressive
-				],
-				eraseallrules: "80" // Erase all rules
-			}
-		},
-		adf: {
-			symbology: "code128",
-			prefix: "^FNC3",
-			postfix: "",
-			enterconfig: "7B1211", // begin new rule
-			exitconfig: "4", // save rule
-			endmessage: "B+", // used after sending text
-			criteria: {
-				stringatposition: {
-					type: "charmap",
-					sendendmessage: true,
-					enterconfig: "6C200",
-					exitconfig: "",
-					prefix: "",
-					postfix: ""
-				},
-				stringatstart: {
-					type: "charmap",
-					sendendmessage: true,
-					enterconfig: "6C201",
-					exitconfig: "",
-					prefix: "",
-					postfix: ""
-				},
-				stringsearch: {
-					type: "charmap",
-					sendendmessage: true,
-					enterconfig: "6C202",
-					exitconfig: "",
-					prefix: "",
-					postfix: ""
-				}
-			},
-			actions: {
-				sendtext: {
-					type: "charmap", // each char in input creates a new barcode
-				},
-				sendcontrol: {
-					type: "multiple", // each char in input creates a new barcode, runs process with one char
-					sendendmessage: true,
-					prefix: "6A1441",
-					process: function(input, adf, callback) { // TODO: change this to callback instead of return
-						// example output: 2=00 A=01 B=02 Z=1A [=1B
-						// non-alphabet
-						if (input == "2") callback("00");
-						if (input == "[") callback("1B");
-						if (input == "\\") callback("1C");
-						if (input == "]") callback("1D");
-						if (input == "6") callback("1E");
-						if (input == "-") callback("1F");
-						
-						input = input.toUpperCase().charCodeAt(0); // convert to ASCII code
-						if (input >= 65 && input <= 90) { // A-Z
-							callback(pad((input - 64).toString(16), 2, "0")); // wait need to convert to hex
-						}
-					}
-				},
-				pauseduration: {
-					type: "single",
-					enterconfig: "30C0D20063",
-					prefix: "A",
-					process: function(input, adf, callback) {
-						// 1.0 duration would be A1, A0
-						callback(Math.floor(input));
-						callback(Math.floor((input % 1).toFixed(1) * 10));
-					}
-				},
-				sendgui: {
-					type: "charmap",
-					prefix: "6A1443"
-				},
-				sendpause: "6A118",
-				sendenter: "6A14470D",
-				sendremaining: "6A110",
-				skipcharacters: {
-					type: "single",
-					prefix: "6A1433",
-					process: function(input, adf, callback) {
-						callback(pad(input, 2, "0"));
-					}
-				}
-			},
-			mapcharacter: {
-				type: "multiple", // each char in input creates a new barcode, runs process with one char
-				process: function(input, adf, callback) { // ADF doesn't take normal keys...
-					// example output: space=20 #=23 $=24 +=2B
-					// straight hex of ASCII
-					callback("B" + pad(input.charCodeAt(0).toString(16).toUpperCase(), 2, "0"));
-				}
-			}
-			
-			
-		},
-		bwippoptions: {
-			parsefnc: true,
-			includetext: true // temp until bwipp inheritance added
-		}
-		
-		// "Using ADF with longer bar codes transmits the bar code in segments of length 252 or less (depending on the host selected), and applies the rule to each segment."
-		// TODO: Parameter to define split support
-	}
-};
-
-var example = {
-	options: [
-		"eraseallrules"
-	],
-	adf: [
-		{
-			criteria: [
-				["stringatstart", "`1"]
-			],
-			actions: [
-				["pauseduration", 1.0], // in seconds
-				["sendgui", "R"],
-				["sendpause"],
-				["sendtext", "cmd"],
-				["sendenter"],
-				["sendpause"],
-				["skipcharacters", 2], // skip over `1
-				["sendremaining"], // send what's left in the barcode
-				["sendenter"]
-			]
-		}
-	],
-	payload: [
-		"`1calc.exe",
-	]
-};
-
-/*
-	Example of a `1 rule that types Win+R, cmd.exe, the barcode, then enter
-
-	"^FNC380",			// erase all rules
-	"^FNC37B1211",		// begin new rule
-	"^FNC36C201",		// specific string at start (max 8)
-	"^FNC3B60",			// `
-	"^FNC3B31",			// 1
-	"^FNC3B+",			// end of message
-	"^FNC330C0D20063",	// specify pause duration
-	"^FNC3A1",			// 1
-	"^FNC3A0",			// 0 (1.0 second delay)
-	"^FNC36A144352",	// send Win + R
-	"^FNC36A118",		// send pause
-	"^FNC3B63",			// c
-	"^FNC3B6D",			// m
-	"^FNC3B64",			// d
-	"^FNC36A14470D",	// send enter
-	"^FNC36A118",		// send pause
-	"^FNC36A143302",	// skip ahead 2 characters (ignoring `1 at start)
-	"^FNC36A110",		// send all data that remains in barcode
-	"^FNC36A14470D",	// send enter
-	"^FNC34"			// save rule
-*/
-
-function addBarcode(text, symbology, options) {
-	var canvas = $("<canvas/>", { id: "barcode-" + barcodeNumber});
-	$("#barcodes").append($("<li>").append(canvas));
-	var rawcanvas = canvas[0];
-	
-	bw.bitmap(new Bitmap);
-	// bw.scale(2,2); // crashes chrome??
-	BWIPP()(bw, symbology, text, options);
-	bw.bitmap().show(rawcanvas, "N"); // "normal"
-	barcodeNumber++;
 }
 
 function arrayEmpty(array) {
@@ -264,8 +73,9 @@ function runModelFunction(params, modelfunc, adf, callback) {
 		});
 	} else if (modelfunc.type == "charmap") {
 		for (var i = 0; i < param.length; i++) {
-			adf.mapcharacter.process(param[i], adf, function callbackextra(code) {
-				// make sure the prefix and postfix make it in
+			runModelFunction(param[i], adf.mapcharacter, adf, function callbackextra(code) {
+				// make sure this prefix and postfix make it in
+				// the mapcharacter may have prefix/postfix too, that's handled by the recursion
 				callback(prefix + code + postfix);
 			});
 		}
@@ -291,9 +101,7 @@ function runModelFunction(params, modelfunc, adf, callback) {
 	
 
 // returns an array of objects with barcode strings and bwipp properties
-function generateBarcodes(model, script) {
-	var codes = [];
-	
+function generateBarcodes(model, script, callback) {
 	if (model.setup) {
 		var currentsymbology;
 		if (model.symbology)
@@ -305,11 +113,7 @@ function generateBarcodes(model, script) {
 		var currentbwippoptions = model.bwippoptions;
 		
 		optionalArray(model.setup.enterconfig, function(code) {
-			codes.push({
-				barcode: model.setup.prefix + code + model.setup.postfix,
-				bwippoptions: currentbwippoptions,
-				symbology: currentsymbology
-			});
+			callback(model.setup.prefix + code + model.setup.postfix, currentsymbology, currentbwippoptions);
 		});
 		
 		// do we have options provided?
@@ -318,11 +122,7 @@ function generateBarcodes(model, script) {
 			if (model.setup.options[input]) {
 				// option in model.setup may be an array
 				optionalArray(model.setup.options[input], function (code) {
-					codes.push({
-						barcode: model.setup.prefix + code + model.setup.postfix,
-						bwippoptions: currentbwippoptions,
-						symbology: currentsymbology
-					});
+					callback(model.setup.prefix + code + model.setup.postfix, currentsymbology, currentbwippoptions);
 				});
 			} else {
 				console.log("Unknown script option: " + input);
@@ -330,11 +130,7 @@ function generateBarcodes(model, script) {
 		});
 		
 		optionalArray(model.setup.exitconfig, function(code) {
-			codes.push({
-				barcode: model.setup.prefix + code + model.setup.postfix,
-				bwippoptions: currentbwippoptions,
-				symbology: currentsymbology
-			});
+			callback(model.setup.prefix + code + model.setup.postfix, currentsymbology, currentbwippoptions);
 		});
 	}
 	
@@ -350,11 +146,7 @@ function generateBarcodes(model, script) {
 		
 		optionalArray(script.adf, function (rule) {
 			optionalArray(model.adf.enterconfig, function(input) {
-				codes.push({
-					barcode: model.adf.prefix + input + model.adf.postfix,
-					bwippoptions: currentbwippoptions,
-					symbology: currentsymbology
-				});
+				callback(model.adf.prefix + input + model.adf.postfix, currentsymbology, currentbwippoptions);
 			});
 			
 			// technically you don't need criteria, omit it and every barcode runs this rule
@@ -373,11 +165,7 @@ function generateBarcodes(model, script) {
 				if (model.adf.criteria[name]) {
 					// run the func in criteria definition
 					runModelFunction(params, model.adf.criteria[name], model.adf, function(code) {
-						codes.push({
-							barcode: model.adf.prefix + code + model.adf.postfix,
-							bwippoptions: currentbwippoptions,
-							symbology: currentsymbology
-						});
+						callback(model.adf.prefix + code + model.adf.postfix, currentsymbology, currentbwippoptions);
 					});
 				} else {
 					console.log("Unknown criteria: " + name);
@@ -402,25 +190,16 @@ function generateBarcodes(model, script) {
 				if (model.adf.actions[name]) {
 					// run the func in action definition
 					runModelFunction(params, model.adf.actions[name], model.adf, function(code) {
-						codes.push({
-							barcode: model.adf.prefix + code + model.adf.postfix,
-							bwippoptions: currentbwippoptions,
-							symbology: currentsymbology
-						});
+						callback(model.adf.prefix + code + model.adf.postfix, currentsymbology, currentbwippoptions);
 					});
 				} else {
 					console.log("Unknown action: " + name);
-					// fatal error
 					return;
 				}
 			});
 			
 			optionalArray(model.adf.exitconfig, function(code) {
-				codes.push({
-					barcode: model.adf.prefix + code + model.adf.postfix,
-					bwippoptions: currentbwippoptions,
-					symbology: currentsymbology
-				});
+				callback(model.adf.prefix + code + model.adf.postfix, currentsymbology, currentbwippoptions);
 			});
 			
 		});
@@ -437,22 +216,7 @@ function generateBarcodes(model, script) {
 		var currentbwippoptions = model.bwippoptions;
 		
 		optionalArray(script.payload, function(code) {
-			codes.push({
-				barcode: code,
-				bwippoptions: currentbwippoptions,
-				symbology: currentsymbology
-			});
+			callback(code, currentsymbology, currentbwippoptions);
 		});
 	}
-	
-	return codes;
 }
-
-$(function () {	
-	var codes = generateBarcodes(models.symbolds6707, example);
-	bw = new BWIPJS(Module, false);
-	for (var i = 0; i < codes.length; i++) {
-		console.log(codes[i].barcode);
-		addBarcode(codes[i].barcode, codes[i].symbology, codes[i].bwippoptions);
-	}
-});
