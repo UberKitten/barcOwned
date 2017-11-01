69075f33ff6a27aba99785d6618b6ce8c3a0a61f
Unnamed Co-conspirator No. 2 <unknown@users.noreply.github.com>
2017-11-01 17:15:58 -0500

    Only show setup script selection in appropriate run modes

--- js/lib/dependent-controls.js
+++ js/lib/dependent-controls.js
@@ -7,22 +7,26 @@ jQuery(($) => {
     const depControl = $(dependentControl)
     const dependency = depControl.data('dependency')
     const targetControl = $(dependency.substring(0, dependency.indexOf('=')))
-    const targetValue = dependency.substring(dependency.indexOf('=') + 1)
+    const targetValues = dependency.substring(dependency.indexOf('=') + 1).split(',')
 
     targetControl.on('change click', (event) => {
       const target = $(event.target)
-      updateDependentControl(depControl, target, targetValue)
+      updateDependentControl(depControl, target, targetValues)
     })
 
-    updateDependentControl(depControl, targetControl, targetValue)
+    updateDependentControl(depControl, targetControl, targetValues)
   })
 
-  function updateDependentControl (depControl, target, targetValue) {
-    if (target.val() === targetValue || target.data('value') === targetValue ||
-      target.parent().val() === targetValue || target.parent().data('value') === targetValue) {
-      depControl.show()
-    } else {
-      depControl.hide()
-    }
+  function updateDependentControl (depControl, target, targetValues) {
+    depControl.hide()
+
+    targetValues.forEach((rawTargetValue) => {
+      const targetValue = rawTargetValue.trim()
+
+      if (target.val() === targetValue || target.data('value') === targetValue ||
+        target.parent().val() === targetValue || target.parent().data('value') === targetValue) {
+        depControl.show()
+      }
+    })
   }
 })
