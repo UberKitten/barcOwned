e24b2ca88ea99e891b723e9dabf1eb2298cab5da
Michael West <michael@mwe.st>
2017-11-02 18:17:01 -0500

    Move endmessage comment and add more info

--- js/lib/barcowned.js
+++ js/lib/barcowned.js
@@ -197,14 +197,14 @@ function runModelFunction (params, modelfunc, adf) {
   }
 
   if (modelfunc.sendendmessage) {
-
+    // in most scenarios I think there should be no prefix/postfix here,
+    // as endmessage is not specific to a single criteria/action
+    // if needed, these should be specified explicity in endmessage
     barcodes.push(adf.endmessage)
   }
 
   // Append exitconfig barcodes to return array
   modelfunc.exitconfig && modelfunc.exitconfig.forEach((code) => {
-    // in most scenarios I think there should be no prefix/postfix here,
-    // as endmessage is not specific to a single criteria/action
     barcodes.push(code)
   })
 
